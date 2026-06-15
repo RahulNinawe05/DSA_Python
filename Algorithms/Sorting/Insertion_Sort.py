caab7f6f454c73fd7bb75082bc704c64399c6ef2
Rahul Ninawe <ninawerahul818@gmail.com>
2026-06-15 23:26:21 +0530

    create file:-add code

---
+++ Algorithms/Sorting/Insertion_Sort.py
@@ -0,0 +1,15 @@
+def Insertion_Sort(nums):
+    n = len(nums)
+    for i in range(1,n):
+        key = nums[i]
+        j = i - 1
+        while j >= 0 and nums[j] >key:
+            nums[j+1] = nums[j]
+            j -= 1
+
+        nums[j+1] = key
+
+    return nums
+
+nums = [3,5,6,4,8,9,10,7,1]
+print(Insertion_Sort(nums))
