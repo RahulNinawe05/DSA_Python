40651a48e8da19e7f6febae2b9ba059281d3d0b1
Rahul Ninawe <ninawerahul818@gmail.com>
2026-07-10 01:04:02 +0530

    add brute force solution

---
+++ Questions/Binary Search/3_Binary Search On Answers/leetcode 1482. Minimum Number of Days to Make m Bouquets.py
@@ -0,0 +1,69 @@
+"""
+You are given an integer array bloomDay, an integer m and an integer k.
+You want to make m bouquets. To make a bouquet, you need to use k adjacent flowers from the garden.
+The garden consists of n flowers, the ith flower will bloom in the bloomDay[i] and then can be used in exactly one bouquet.
+Return the minimum number of days you need to wait to be able to make m bouquets from the garden. If it is impossible to make m bouquets return -1.
+Example 1:
+
+Input: bloomDay = [1,10,3,10,2], m = 3, k = 1
+Output: 3
+Explanation: Let us see what happened in the first three days. x means flower bloomed and _ means flower did not bloom in the garden.
+We need 3 bouquets each should contain 1 flower.
+After day 1: [x, _, _, _, _]   // we can only make one bouquet.
+After day 2: [x, _, _, _, x]   // we can only make two bouquets.
+After day 3: [x, _, x, _, x]   // we can make 3 bouquets. The answer is 3.
+Example 2:
+
+Input: bloomDay = [1,10,3,10,2], m = 3, k = 2
+Output: -1
+Explanation: We need 3 bouquets each has 2 flowers, that means we need 6 flowers. We only have 5 flowers so it is impossible to get the needed bouquets and we return -1.
+Example 3:
+
+Input: bloomDay = [7,7,7,7,12,7,7], m = 2, k = 3
+Output: 12
+Explanation: We need 2 bouquets each should have 3 flowers.
+Here is the garden after the 7 and 12 days:
+After day 7: [x, x, x, x, _, x, x]
+We can make one bouquet of the first three flowers that bloomed. We cannot make another bouquet from the last three flowers that bloomed because they are not adjacent.
+After day 12: [x, x, x, x, x, x, x]
+It is obvious that we can make two bouquets in different ways.
+
+"""
+
+
+bloomDay = [7, 7, 7, 7, 13, 11, 12, 7]
+m = 2
+k = 3
+
+min_value = min(bloomDay)
+max_value = max(bloomDay)
+
+answer = -1
+
+for day in range(min_value, max_value+1):
+
+    cout = 0
+    bouquet = 0
+    for i in range(0, len(bloomDay)):
+
+        # flower are bloom 
+        if bloomDay[i] <= day:
+            cout += 1
+
+            # find out k adjacent flowers
+            if cout == k:
+                bouquet += 1
+                cout = 0
+
+        # flower are not bloom,the adjacency break
+        else:
+            cout = 0
+
+    # required bouquets are found
+    if bouquet >= m:
+        answer = day
+        break
+
+print(answer)
+
+
