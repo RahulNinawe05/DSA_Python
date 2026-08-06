a3f05412b9dbaed0c4bb99e6e05b02bdb1554288
Rahul Ninawe <ninawerahul818@gmail.com>
2026-08-06 23:59:21 +0530

    revised & add brute as well as better solution

--- Questions/Two Pointers/leetcode 167. Two Sum II - Input Array Is Sorted.py
+++ Questions/Two Pointers/leetcode 167. Two Sum II - Input Array Is Sorted.py
@@ -1,18 +1,87 @@
-def twoSum(numbers, target):
-    l = 0
-    r = len(numbers) - 1
+"""
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array [index1, index2] of length 2.
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+Your solution must use only constant extra space.
 
-    while l < r:
-        s = numbers[l] + numbers[r]
+Example 1:
 
-        if s == target:
-            return [l + 1, r + 1]
-        elif s > target:
-            r -= 1
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+"""
+
+
+# Brute Solution 
+#TC: O(n^2) | SC: O(1)
+def twoSum_Brute_solution(numbers, target):
+    n = len(numbers)
+    for i in range(0,n):
+        for j in range(i+1,n):
+            if numbers[i] + numbers[j] == target:
+                return i+1,j+1
+    return []
+
+
+# Better Solution (Binary Search)
+#TC: O(n log n) | SC: O(1)
+def twoSum_Better_solution(numbers, target):
+
+    for i in range(0,len(numbers)):
+        x = target - numbers[i] 
+
+        left = i + 1
+        right = len(numbers) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if numbers[mid] == x:
+                return i+1, mid +1
+
+            elif numbers[mid] < x:
+                left = mid + 1
+
+            else:
+                right = mid - 1
+
+    return []
+
+
+# Optimal Solution (Two Pointer)
+#TC: O(n) | SC: O(1)
+def twoSum_Optimal_solution(numbers, target):
+    left = 0
+    right = len(numbers) - 1
+
+    while left < right:
+        sum = numbers[left] + numbers[right]
+
+        if sum == target:
+            return left+ 1, right + 1
+
+        elif sum < target:
+            left += 1
+
         else:
-            l += 1
+            right -= 1
 
-numbers = [1,2,3,4,5,6,7,8]
-target = 11
+    return []
 
-print(twoSum(numbers,target))
+
+numbers = [2,7,11,15]
+target = 9
+
+print(twoSum_Brute_solution(numbers,target))
+print(twoSum_Better_solution(numbers,target))
+print(twoSum_Optimal_solution(numbers,target))
